a983675b50d263082f9b738f32141be4ee0cf078
sam-lo <lotsznok0720@icloud.com>
2022-01-29 14:50:38 +0800

    Initial Commit

---
+++ Vita/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vita
+//
+//  Created by Sam on 29/1/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vita/VitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  VitaApp.swift
+//  Vita
+//
+//  Created by Sam on 29/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct VitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
